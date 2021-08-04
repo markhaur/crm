a3b1ddf1aa7111faa1b91d9813aa787ca67a28f2
Faisal Nisar <fnisar394@protonmail.com>
2021-08-05 00:43:57 +0500

    middleware was added in app.routes. more than one middleware can be added

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -2,7 +2,15 @@
 const routes = (app) => {
 
     app.route('/contact')
-    .get((req, res) => {
+    .get((req, res, next) => {
+        // this is the middleware
+        console.log(`Request from: ${req.originalUrl}`);
+        console.log(`Request type: ${req.method}`);
+        next();
+    }, (req, res, next) => {
+        console.log('this is my another middleware');
+        next();
+    }, (req, res) => {
         res.send('GET request successful');
     })
 
